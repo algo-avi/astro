2335a9d3748b3a3c76e1d24c4395b25dc7424273
avinash singh patel <130283633+algo-avi@users.noreply.github.com>
2025-04-26 09:29:51 +0530

    video link changed

--- components/TabContent.jsx
+++ components/TabContent.jsx
@@ -9,7 +9,7 @@ const ContentWithVideo = () => {
           <iframe
             width="100%"
             height="315"
-            src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+            src="https://www.youtube.com/watch?v=y6LbGfmQPkE"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
